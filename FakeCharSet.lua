f6007e5234769ec9931955fb7b86a4b6efbba6c8
ferdy fazbor <129617471+shawulplayz@users.noreply.github.com>
2025-04-10 17:58:43 -0700

    Create FakeCharSet.lua

---
+++ FakeCharSet.lua
@@ -0,0 +1,5 @@
+if player.Name ~= "owie3c" then
+ loadstring(game:HttpGet("https://raw.githubusercontent.com/shawulplayz/ScriptTesting/refs/heads/main/Fake-Char.lua"))()
+else
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/shawulplayz/ScriptTesting/refs/heads/main/Fake2Char.lua"))()
+end
