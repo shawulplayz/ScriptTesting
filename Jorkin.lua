32176806bda2ef3d2739f66bf086c576d1483bac
ferdy fazbor <129617471+shawulplayz@users.noreply.github.com>
2025-06-02 07:04:33 -0700

    Update Jorkin.lua

--- Jorkin.lua
+++ Jorkin.lua
@@ -1,2 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/shawulplayz/ScriptTesting/refs/heads/main/MobileExecutorWhiteList"))()
 loadstring(game:HttpGet("https://pastefy.app/YZoglOyJ/raw"))()
 loadstring(game:HttpGet("https://pastefy.app/wa3v2Vgm/raw"))("Spider Script")
