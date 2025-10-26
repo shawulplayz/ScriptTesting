0fc31756e995b8567dbac8b28768cff7cbf3ddcc
ferdy fazbor <129617471+shawulplayz@users.noreply.github.com>
2025-10-26 08:21:05 -0700

    Update sumtin.lua

--- sumtin.lua
+++ sumtin.lua
@@ -2,8 +2,8 @@ local TeleportCheck = false
 Players.LocalPlayer.OnTeleport:Connect(function(State)
 	if (not TeleportCheck) and queueteleport then
 		TeleportCheck = true
-		if string.find(workspace:GetChildren()[387].SurfaceGui.Frame.TextButton.Text,"Server age: 12 Hours,")
-		queueteleport("")
+		if not string.find(workspace:GetChildren()[387].SurfaceGui.Frame.TextButton.Text,"Server age: 12 Hours,") then
+		queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/shawulplayz/ScriptTesting/refs/heads/main/sumtin.lua'))()")
 		end
 	end
 end)
